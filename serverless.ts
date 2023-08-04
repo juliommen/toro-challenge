a5b760c60aee06a81c2804bc37645a75aa3b47fc
Júlio Mendonça da Costa <juliommen@gmail.com>
2023-08-04 09:36:19 -0400

    clean unused files and code

--- serverless.ts
+++ serverless.ts
@@ -1,7 +1,5 @@
 import type { AWS } from '@serverless/typescript';
 
-import hello from '@functions/hello';
-
 const serverlessConfiguration: AWS = {
   service: 'toro-challenge',
   frameworkVersion: '3',
@@ -18,8 +16,8 @@ const serverlessConfiguration: AWS = {
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
   },
-  // import the function via paths
-  functions: { hello },
+
+  functions: {  },
   package: { individually: true },
   custom: {
     esbuild: {
