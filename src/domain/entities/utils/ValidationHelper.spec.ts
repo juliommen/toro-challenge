7a324683f5be5cd6f41e4eb146a4efb17cb48d78
Júlio Mendonça da Costa <juliommen@gmail.com>
2023-08-04 17:37:35 -0400

    add more unit tests to validation helper

--- src/domain/entities/utils/ValidationHelper.spec.ts
+++ src/domain/entities/utils/ValidationHelper.spec.ts
@@ -5,8 +5,10 @@ import { ValidatorHelper } from './ValidationHelper'
 describe('User account unit tests', () => {
   it('should be able to correctly check if a value cannot be converted to integer', async () => {
     expect(ValidatorHelper.checkConvertionToInteger('3-35')).toEqual(false)
+    expect(ValidatorHelper.checkConvertionToInteger('1a')).toEqual(false)
+    expect(ValidatorHelper.checkConvertionToInteger('1.1')).toEqual(false)
+    expect(ValidatorHelper.checkConvertionToInteger(1 as any)).toEqual(false)
     expect(ValidatorHelper.checkConvertionToInteger('')).toEqual(false)
-    expect(ValidatorHelper.checkConvertionToInteger(1.1 as any)).toEqual(false)
     expect(ValidatorHelper.checkConvertionToInteger({} as any)).toEqual(false)
     expect(ValidatorHelper.checkConvertionToInteger([] as any)).toEqual(false)
     expect(ValidatorHelper.checkConvertionToInteger(undefined)).toEqual(false)
@@ -21,5 +23,15 @@ describe('User account unit tests', () => {
     expect(ValidatorHelper.checkCpfValidation(null as any)).toEqual(false)
     expect(ValidatorHelper.checkCpfValidation([] as any)).toEqual(false)
     expect(ValidatorHelper.checkCpfValidation({} as any)).toEqual(false)
+  })
+
+  it('should be able to correctly check a valid CPF', async () => {
+    expect(ValidatorHelper.checkCpfValidation('36749033008')).toEqual(true)
+    expect(ValidatorHelper.checkCpfValidation('89186073001')).toEqual(true)
+    expect(ValidatorHelper.checkCpfValidation('23780065002')).toEqual(true)
+    expect(ValidatorHelper.checkCpfValidation('99993430072')).toEqual(true)
+    expect(ValidatorHelper.checkCpfValidation('89074757090')).toEqual(true)
+    expect(ValidatorHelper.checkCpfValidation('08621743094')).toEqual(true)
+    expect(ValidatorHelper.checkCpfValidation('93886334074')).toEqual(true)
   })
 })
