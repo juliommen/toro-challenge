53782c6abc3c4c5287a5fb523682e5189caef22b
Júlio Mendonça da Costa <juliommen@gmail.com>
2023-08-05 18:16:42 -0400

    add create user account dto and controller

--- src/http/controllers/create-user-account.ts
+++ src/http/controllers/create-user-account.ts
@@ -1,3 +1,15 @@
 import { Request, Response } from 'express'
+import { ICreateUserAccountDTO } from '../dtos/ICreateUserAccountDTO'
+import { UserAccount } from '@/domain/entities/UserAccount'
+import { makeCreateUserAccountUseCase } from '@/factories/create-user-account-use-case'
 
-export function createUserAccountController(req: Request, res: Response) {}
+export async function createUserAccountController(req: Request, res: Response) {
+  const { cpf } = req.body as ICreateUserAccountDTO
+  const userAccount = new UserAccount(cpf)
+
+  const createUserAccountUseCase = makeCreateUserAccountUseCase()
+
+  const createdAccount = await createUserAccountUseCase.execute(userAccount)
+
+  return res.status(201).send({ createdAccount })
+}
---
+++ src/http/dtos/ICreateUserAccountDTO.ts
@@ -0,0 +1,3 @@
+export interface ICreateUserAccountDTO {
+  cpf: string
+}
