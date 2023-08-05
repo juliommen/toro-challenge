355b49e98593111cbc0997b4f10092f009da0e48
Júlio Mendonça da Costa <juliommen@gmail.com>
2023-08-05 14:40:23 -0400

    add balance on return of investment transaction

--- src/domain/interfaces/ITransactionsRepository.ts
+++ src/domain/interfaces/ITransactionsRepository.ts
@@ -1,12 +1,16 @@
 import { InvestmentTransacition } from '../entities/InvestmentTransaction'
 import { TransferTransacition } from '../entities/TransferTransaction'
 
+interface CreateInvestmentResponse {
+  investmentTransaction: InvestmentTransacition
+  balance: number
+}
+
 export interface ITransactionsRepository {
   createTransfer(
     transferTransaction: TransferTransacition,
   ): Promise<TransferTransacition>
   createInvestment(
     investmentTransaction: InvestmentTransacition,
-  ): Promise<InvestmentTransacition>
-  getBalance(accountNumber: number): Promise<number>
+  ): Promise<CreateInvestmentResponse>
 }
