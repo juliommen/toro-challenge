7ce2e682cc297dccebc0a97a8b7198c8d8d764da
Júlio Mendonça da Costa <juliommen@gmail.com>
2023-08-05 19:59:53 -0400

    change return type of createInvestment repos

--- src/domain/interfaces/ITransactionsRepository.ts
+++ src/domain/interfaces/ITransactionsRepository.ts
@@ -1,16 +1,11 @@
 import { InvestmentTransaction } from '../entities/InvestmentTransaction'
 import { TransferTransaction } from '../entities/TransferTransaction'
 
-interface CreateInvestmentResponse {
-  investmentTransaction: InvestmentTransaction
-  balance: number
-}
-
 export interface ITransactionsRepository {
   createTransfer(
     transferTransaction: TransferTransaction,
   ): Promise<TransferTransaction>
   createInvestment(
     investmentTransaction: InvestmentTransaction,
-  ): Promise<CreateInvestmentResponse>
+  ): Promise<InvestmentTransaction>
 }
