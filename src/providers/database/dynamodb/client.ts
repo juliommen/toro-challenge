ba223528ff280d88fbfbf83c813318591ec5aa37
Júlio Mendonça da Costa <juliommen@gmail.com>
2023-08-05 17:08:36 -0400

    renamed dynamodb client

--- src/providers/database/dynamodb/client.ts
+++ src/providers/database/dynamodb/client.ts
@@ -11,6 +11,6 @@ const options = {
 
 const isOffline = process.env.IS_OFFLINE
 
-export const document = isOffline
+export const DynamoClient = isOffline
   ? new DynamoDB.DocumentClient(options)
   : new DynamoDB.DocumentClient()
