1824cf0c416431a3c13b5a236d8aa9477c015171
Júlio Mendonça da Costa <juliommen@gmail.com>
2023-08-06 19:02:41 -0400

    import dotenv for build

--- src/http/app.ts
+++ src/http/app.ts
@@ -1,4 +1,5 @@
 import 'express-async-errors'
+import 'dotenv/config'
 import express, { NextFunction, Request, Response } from 'express'
 import { router } from './routes'
 import { AppError } from '@/domain/errors/AppError'
